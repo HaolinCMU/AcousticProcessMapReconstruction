e7d61c9edd4101c3373cfe04cc851e5d84c88552
HaolinCMU <haolinl@andrew.cmu.edu>
2022-08-10 12:21:54 -0400

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 03:39:29 2022
+
+@author: hlinl
+"""
+
+
+import copy
+import glob
+import os
+import math
+
+import numpy as np
+import scipy
+import torch
+import torch.nn as nn
+import torch.utils
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.image as mig
+import PIL
+
+from collections import defaultdict
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Compose, Resize, ToTensor
+
+from PARAM import *
+
+
+class AcousticSpectrumDefectDataset(Dataset):
+    """
+    """
+
+    def __init__(self, spectrum_dir, spectrum_extension=SPECTRUM_EXTENSION, dtype=torch.float32, 
+                 img_transform=Compose([Resize(IMG_SIZE), ToTensor()])):
+        """
+        """
+
+        self.spectrum_dir = spectrum_dir
+        self.spectrum_extension = spectrum_extension
+        self.dtype = dtype
+        self.img_transform = img_transform
+
+        self._dataset = defaultdict()
+        self._dataset_size = 0
+    
+
+    def __len__(self):
+        """
+        """
+
+        return self._dataset_size
+
+    
+    def __getitem__(self, id):
+        """
+        """
+
+        
+    def _set_data_repo(self):
+        """
+        """
+
+        spectrum_folder_list = os.listdir(self.spectrum_dir)
+
+        for spectrum_folder in spectrum_folder_list:
+            spectrum_folder_path = os.path.join(self.spectrum_dir, spectrum_folder)
+            spectrum_path_list_temp = glob.glob(os.path.join(spectrum_folder_path, "*.{}".format(self.spectrum_extension)))
+
+            # Establish image label dict. 
+            for i, path in enumerate(spectrum_path_list_temp):
+                self._dataset[str(int(input_image_accum_num+i))] = [input_image_subfolder_name_temp, i, path]
+
+            input_image_accum_num += len(input_image_path_list_temp)
+            
+        
+
+
+class AcousticSpectrumVisualDataset(Dataset):
+    """
+    """
+
+    def __init__(self, spectrum_dir, meltpool_dir):
+        """
+        """
+
+        pass
+
+
+class FrameAutoencoderDataset(Dataset):
+    """
+    """
+
+    def __init__(self, input_data_dir, output_data_dir, img_pattern=IMG.IMAGE_EXTENSION, dtype=torch.float32, 
+                 input_image_transform=Compose([Resize(ML_VAE.INPUT_IMAGE_SIZE), ToTensor()]), 
+                 output_image_transform=Compose([Resize(ML_VAE.OUTPUT_IMAGE_SIZE), ToTensor()])):
+        """
+        Expected data directory structure: folder (data_dir) -> subfolders (layers) -> images (frames). 
+        """
+
+        self.input_data_dir = input_data_dir # The total directory of all input images for the autoencoder. 
+        self.output_data_dir = output_data_dir # The total directory of all output images for the autoencoder. 
+        self.img_pattern = img_pattern
+        self.image_dtype = dtype
+        self.input_image_transform = input_image_transform
+        self.output_image_transform = output_image_transform
+        
+        # Data repos (path lists) & Data label dictionaries. 
+        self._dataset_size = 0 
+        self._input_image_label_dict = {} # {`ind`->int: [`subfolder`->str, `image_No`->int, `image_path`-> str]}. 
+        self._output_image_label_dict = {} # {`ind`->int: [`subfolder`->str, `image_No`->int, `image_path`-> str]}. 
+
+        self._set_data_repo()
+    
+
+    def __len__(self):
+        """
+        """
+
+        return self._dataset_size
+
+
+    def __getitem__(self, index):
+        """
+        """
+
+        index = copy.deepcopy(str(int(index)))
+
+        # input_img = PIL.Image.open(self._input_image_label_dict[index][2])
+        # output_img = PIL.Image.open(self._output_image_label_dict[index][2])
+
+        input_img = PIL.Image.fromarray(np.uint8(mig.imread(self._input_image_label_dict[index][2])*255))
+        output_img = PIL.Image.fromarray(np.uint8(mig.imread(self._output_image_label_dict[index][2])*255))
+
+        if self.input_image_transform:
+            input_img = copy.deepcopy(self.input_image_transform(input_img).to(self.image_dtype)) # Transformed tensor of prescribed data type. [c, h, w]. 
+        if self.output_image_transform:
+            output_img = copy.deepcopy(self.output_image_transform(output_img).to(self.image_dtype)) # Transformed tensor of prescribed data type. [c, h, w]. 
+
+        # # ---------- Reshape tensors to make them compatible with NN ---------- 
+        # input_img_c, input_img_h, input_img_w = input_img.size()
+        # output_img_c, output_img_h, output_img_w = output_img.size()
+
+        # input_img = input_img.view(-1, input_img_c, input_img_h, input_img_w)
+        # output_img = output_img.view(-1, output_img_c, output_img_h, output_img_w)
+
+        sample = {'input': input_img, 'output': output_img}
+
+        return sample
+    
+
+    @property
+    def input_data_repo_dict(self):
+        """
+        """
+
+        return self._input_image_label_dict
+    
+
+    @property
+    def output_data_repo_dict(self):
+        """
+        """
+
+        return self._output_image_label_dict
+    
+
+    def _set_data_repo(self):
+        """
+        """
+
+        # ---------- Set input data repo ---------- 
+        input_image_subfolder_list = os.listdir(self.input_data_dir)
+        input_image_subdir_list = glob.glob(os.path.join(self.input_data_dir, "*"))
+        input_image_accum_num = 0 # Used for establishing the image label dict. 
+
+        for ind, input_image_subdir in enumerate(input_image_subdir_list):
+            input_image_subfolder_name_temp = input_image_subfolder_list[ind]
+            input_image_path_list_temp = copy.deepcopy(glob.glob(os.path.join(input_image_subdir, 
+                                                                              "*.{}".format(self.img_pattern))))
+
+            # Establish image label dict. 
+            for i, path in enumerate(input_image_path_list_temp):
+                self._input_image_label_dict[str(int(input_image_accum_num+i))] = [input_image_subfolder_name_temp, i, path]
+
+            input_image_accum_num += len(input_image_path_list_temp)
+
+        # ---------- Set output data repo ---------- 
+        output_image_subfolder_list = os.listdir(self.output_data_dir)
+        output_image_subdir_list = glob.glob(os.path.join(self.output_data_dir, "*"))
+        output_image_accum_num = 0 # Used for establishing the image label dict. 
+
+        for ind, output_image_subdir in enumerate(output_image_subdir_list):
+            output_image_subfolder_name_temp = output_image_subfolder_list[ind]
+            output_image_path_list_temp = copy.deepcopy(glob.glob(os.path.join(output_image_subdir, 
+                                                                               "*.{}".format(self.img_pattern))))
+
+            # Establish image label dict. 
+            for i, path in enumerate(output_image_path_list_temp):
+                self._output_image_label_dict[str(int(output_image_accum_num+i))] = [output_image_subfolder_name_temp, i, path]
+
+            output_image_accum_num += len(output_image_path_list_temp)
+        
+        self._dataset_size = min(input_image_accum_num, output_image_accum_num)
+
+
+    def extract(self, ind_array):
+        """
+        Return info of input and output data that corresponds to the given `ind_array` with the exact order. 
+        """
+
+        ind_array = copy.deepcopy(ind_array.astype(str).reshape(-1))
+        
+        # Input. 
+        input_image_subfolder_list = [self._input_image_label_dict[ind][0] for ind in ind_array]
+        input_image_ind_list = [self._input_image_label_dict[ind][1] for ind in ind_array]
+        input_image_path_list = [self._input_image_label_dict[ind][2] for ind in ind_array]
+
+        # Output. 
+        output_image_subfolder_list = [self._output_image_label_dict[ind][0] for ind in ind_array]
+        output_image_ind_list = [self._output_image_label_dict[ind][1] for ind in ind_array]
+        output_image_path_list = [self._output_image_label_dict[ind][2] for ind in ind_array]
+
+        return (input_image_subfolder_list, input_image_ind_list, input_image_path_list,
+                output_image_subfolder_list, output_image_ind_list, output_image_path_list)
+
+
+
+if __name__ == "__main__":
+    """
+    """
+
+    pass
--- model.py
+++ model.py
@@ -7,24 +7,23 @@ Created on Thu Aug  4 04:27:17 2022
 
 
 import os
+import gc
 import glob
 import sys
 import time
 
 import copy
+from xml.dom.expatbuilder import parseString
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.utils
 import torch.nn.functional as F
-import tensorflow as tf
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pywt
 
-from collections import defaultdict
-from datetime import datetime
 from torch.utils.data import Dataset, DataLoader
 
 from PARAM import *
@@ -32,9 +31,10 @@ from PARAM import *
 
 class CNN_2D(nn.Module):
     """
+    A simple parameterized 2D CNN model generator. 
     """
 
-    def __init__(self, in_channel_num, output_dim, conv_hidden_layer_num, mlp_hidden_layer_num):
+    def __init__(self, in_channel_num, output_dim, conv_hidden_layer_num, mlp_hidden_layer_struct=MLP_HIDDEN_LAYER_STRUCT):
         """
         Conv block first, then MLP block. 
         """
@@ -43,63 +43,136 @@ class CNN_2D(nn.Module):
         self.in_channel_num = in_channel_num
         self.output_dim = output_dim
         self.conv_hidden_layer_num = conv_hidden_layer_num
-        self.mlp_hidden_layer_num = mlp_hidden_layer_num
+        self.mlp_hidden_layer_struct = mlp_hidden_layer_struct
+        self.mlp_hidden_layer_num = len(self.mlp_hidden_layer_struct)
         
-        self._hidden_architecture = None
+        self._conv_net = None
+        self._mlp_net = None
 
+        self._is_conv_net = True if self.conv_hidden_layer_num != 0 else False
+        self._is_mlp_net = True if self.mlp_hidden_layer_num != 0 else False
+
+        self._build()
+
+
+    @property
+    def conv_net(self):
+        """
+        """
+
+        if self._conv_net is not None: return self._conv_net
+        else: raise ValueError("Conv. net not created. ")
+    
+
+    @property
+    def mlp_net(self):
+        """
+        """
+
+        if self._mlp_net is not None: return self._mlp_net
+        else: raise ValueError("MLP net not created. ")
     
 
     def forward(self, x):
         """
         """
 
-        pass
+        if self._conv_net is None or not self._is_conv_net:
+            raise ValueError("Neural network model not generated. ")
+        
+        conv_encode = self._conv_net(x)
+        output = conv_encode.view(conv_encode.size(0), -1)
+        if self._is_mlp_net: output = self._mlp_net(output)
+
+        return output
 
 
-    def _img_dim_conv(self):
+    def _after_conv_img_dim(self, in_dim, conv_kernel_size, conv_padding, conv_stride):
         """
         """
+
+        return int((in_dim + 2*conv_padding - conv_kernel_size) / conv_stride + 1)
+
+
+    def _after_pool_img_dim(self, in_dim, pool_kernel_size, pool_padding, pool_stride):
+        """
+        """
+
+        return int((in_dim + 2*pool_padding - pool_kernel_size) / pool_stride + 1)
 
     
-    def architecture(self):
+    def _conv_block(self):
         """
         """
 
         architecture_pre_module = []
 
-        # Conv. block. 
         for i in range(self.conv_hidden_layer_num):
             if i == 0: 
                 in_channel_num_temp = self.in_channel_num
                 out_channel_num_temp = FIRST_CONV_CHANNEL_NUM
+                img_size_temp = IMG_SIZE
+                output_img_dim_temp = None
             else: 
                 in_channel_num_temp = copy.deepcopy(out_channel_num_temp)
                 out_channel_num_temp = copy.deepcopy(int(in_channel_num_temp*2))
+                img_size_temp = copy.deepcopy(output_img_dim_temp)
             
             architecture_pre_module.append(nn.Conv2d(in_channel_num_temp, out_channel_num_temp, kernel_size=CONV_KERNEL_SIZE, 
                                                      stride=CONV_STRIDE_SIZE, padding=CONV_PADDING_SIZE))
-            # architecture_pre_module.append(nn.Dropout(0.5))
-            architecture_pre_module.append(nn.BatchNorm2d(out_channel_num_temp))
+            # architecture_pre_module.append(nn.Dropout(0.5)) # Dropout layer. 
             architecture_pre_module.append(ACTIVATION_LAYER)
-            architecture_pre_module.append(POOLING_LAYER(POOLING_KERNEL_SIZE, stride=POOLING_STRIDE_SIZE, 
-                                                         padding=POOLING_PADDING_SIZE))
+            architecture_pre_module.append(nn.BatchNorm2d(out_channel_num_temp))
             
-        out_channel_num_temp * IMG_SIZE
+            after_conv_img_dim = self._after_conv_img_dim(img_size_temp, conv_kernel_size=CONV_KERNEL_SIZE, 
+                                                          conv_padding=CONV_PADDING_SIZE, conv_stride=CONV_STRIDE_SIZE)
+
+            if i != self.conv_hidden_layer_num - 1:
+                architecture_pre_module.append(POOLING_LAYER(POOLING_KERNEL_SIZE, stride=POOLING_STRIDE_SIZE, 
+                                                             padding=POOLING_PADDING_SIZE))
+                output_img_dim_temp = self._after_pool_img_dim(after_conv_img_dim, pool_kernel_size=POOLING_KERNEL_SIZE, 
+                                                               pool_padding=POOLING_PADDING_SIZE, pool_stride=POOLING_STRIDE_SIZE)
+            else: 
+                architecture_pre_module.append(nn.AvgPool2d(after_conv_img_dim)) # Global average pooling. 
+                output_img_dim_temp = self._after_pool_img_dim(after_conv_img_dim, pool_kernel_size=after_conv_img_dim, 
+                                                               pool_padding=0, pool_stride=1)
+
+        self._conv_net = nn.Sequential(*architecture_pre_module)
+        out_dim = int(out_channel_num_temp * output_img_dim_temp**2)
+
+        return out_dim # Ideally should be identical to `out_channel_num_temp`. 
+    
+
+    def _mlp_block(self, in_dim):
+        """
+        """
+
+        architecture_pre_module = []
+
+        for i in range(self.mlp_hidden_layer_num):
+            if i == 0: 
+                in_dim_temp = in_dim
+                output_dim_temp = self.mlp_hidden_layer_struct[0]
+            else: 
+                in_dim_temp = copy.deepcopy(output_dim_temp)
+                output_dim_temp = self.mlp_hidden_layer_struct[i]
+
+            architecture_pre_module.append(nn.Linear(in_dim_temp, output_dim_temp))
+            # architecture_pre_module.append(nn.Dropout(0.5)) # Dropout layer. 
+            if i != self.mlp_hidden_layer_num - 1: architecture_pre_module.append(ACTIVATION_LAYER)
+        
+        self._mlp_net = nn.Sequential(*architecture_pre_module)
 
 
-            
-            hidden_block_temp = nn.Sequential(nn.Conv2d(in_channel_num_temp, out_channel_num_temp, kernel_size=CONV_KERNEL_SIZE, 
-                                                        stride=CONV_STRIDE_SIZE, padding=CONV_PADDING_SIZE), 
-                                            #   nn.Dropout(0.5), 
-                                              nn.BatchNorm2d(out_channel_num_temp), 
-                                              ACTIVATION_LAYER,
-                                              )
-            pooling_layer = POOLING_LAYER(POOLING_KERNEL_SIZE, stride=POOLING_STRIDE_SIZE)
+    def _build(self):
+        """
+        """
 
-            architecture_pre_module.append(hidden_block_temp)
-            architecture_pre_module.append(pooling_layer)
-        
-        
+        conv_out_dim = None
 
-        
-        
+        if self._is_conv_net: 
+            conv_out_dim = self._conv_block()
+            if self._is_mlp_net: self._mlp_block(conv_out_dim)
+        else: pass
+
+
